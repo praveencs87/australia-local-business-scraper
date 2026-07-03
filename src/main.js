b04519118dcfad3442831c05ad2abe83dacd914d
CoderBunch <praveencs87@gmail.com>
2026-07-03 09:33:33 +0530

    chore: enforce PPE, memory limits, and auto-abort

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { armKillSwitch, disarmKillSwitch } from './utils/timeoutManager.js';
 import { Actor, log } from 'apify';
 import { PlaywrightCrawler } from 'crawlee';
 
@@ -144,7 +145,9 @@ try {
         url: 'https://www.local.com.au/'
     }]);
 
+    armKillSwitch(crawler);
     await crawler.run();
+    disarmKillSwitch();
 
     log.info(`🎉 Done! Extracted ${extractedCount} Australian Business leads.`);
 
